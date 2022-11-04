f7382bcf66bd758cbbf3eabc775a3da8db5381f5
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2022-11-03 23:47:33 -0400

    boss //TODO

--- src/Game/Entities/Enemies/include/Boss.h
+++ src/Game/Entities/Enemies/include/Boss.h
@@ -12,6 +12,14 @@ public:
     ~Boss();
 private:
 
+// Boss: Implement a Boss class. This class must inherit from the Enemy
+// class. Bosses appear in an Area after all other Enemies of that area are
+// defeated. A Boss Battle must have at least two phases. That is to say, you
+// must deplete the Boss’s health bar twice in order to beat them. Defeating
+// a Boss moves the Player to the next Area or a Win State if there are no
+// remaining Areas. You must have at least one Boss instance per Area of
+// the game
+
 };
 
 #endif
